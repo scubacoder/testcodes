3aa4d1886f469e208f250925bd546a942ca0ce85
unknown <vinceaycardo@gmail.com>
2014-05-19 16:16:28 +0800

    phpmailer html with attachment success

--- phpmailer/index.php
+++ phpmailer/index.php
@@ -5,23 +5,29 @@ require 'vendor/autoload.php';
 //use PHPMailer;
 
 $mail = new PHPMailer;
+$body = file_get_contents('content.html');
+$body = eregi_replace("[\]", "", $body);
 
 $mail->isSMTP();	// use SMTP
 $mail->Host = "smtp.gmail.com";	// SMTP server
 $mail->Port = 587;
 $mail->SMTPAuth = TRUE;
 $mail->SMTPSecure = "tls";
-$mail->Username = "vincentaycardo@gmail.com";
-$mail->Password = "vinc3gmail";
+$mail->Username = "vinceaycardo@gmail.com";
+$mail->Password = "alic3marie";
 
 $mail->setFrom('vincentaycardo@gmail.com', 'Vince Gmail');
 $mail->addReplyTo('vincentaycardo@gmail.com', 'Vince Gmail');
 
-$mail->Subject = "PHPMailer test email 1";
-$mail->Body = "Hi, this is email 1 from phpmailer.";
+$mail->Subject = "PHPMailer HTML email with attachment";
+//$mail->Body = "Hi, this is email 1 from phpmailer.";
+$mail->AltBody = "To view this message, use an HTML compatible email.";
+$mail->msgHTML($body);
 
 $address = "vinceaycardo@outlook.com";
 $mail->addAddress($address, 'Vince Outlook');
+
+$mail->addAttachment("dog.png");
 
 if (! $mail->send()){
 	echo "Mailer Error: ".$mail->ErrorInfo;
