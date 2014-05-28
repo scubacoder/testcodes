b249a38f293b2939eae5233c142210be32126f0e
scubacoder <vinceaycardo@gmail.com>
2014-05-28 23:35:05 +0800

    new client creation for FB

--- freshbooks/newClient.php
+++ freshbooks/newClient.php
@@ -4,24 +4,22 @@ include_once 'include/init.php';
 //include particular file for entity you need (Client, Invoice, Category...)
 include_once LIB_PATH . "/FreshBooks/Client.php";
 
-//$clientId = 142596;
-$clientId = 142802;
-
 //new Client object
 $client = new FreshBooks_Client();
 
+// populate properties
+$client->organization = "Cebu City Health";
+$client->email = "alice@mrlemail.com";
+$client->firstName = "Alicemarie";
+$client->lastName = "Aycardo";
+
 //try to get client with client_id $clientId
-if(!$client->get($clientId)){
+if(!$client->create()){
 	//no data - read error
 	echo $client->lastError;
 } else {
 	//investigate populated data
 	print_r($client);
 	echo "<hr>";
-	// Option #1 call via object
-	echo $client->organization."<br>". $client->firstName ." ". $client->lastName." (". $client->clientId .")";
-	// Option #2 convert object to array
-	//$myarr = (array) $client;
-	//echo $myarr['clientId']."<br>";
-	//echo $myarr['lastName'].", ".$myarr['firstName']."<br>";
+	echo $client->clientId;
 }
