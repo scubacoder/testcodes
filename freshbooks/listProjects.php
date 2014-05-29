014145d0307353b09c985e7772b3b24058d67346
scubacoder <vinceaycardo@gmail.com>
2014-05-29 23:35:25 +0800

    project list FB

--- freshbooks/listProjects.php
+++ freshbooks/listProjects.php
@@ -2,36 +2,15 @@
 include_once 'include/init.php';
 
 //include particular file for entity you need (Client, Invoice, Category...)
-include_once LIB_PATH . "/FreshBooks/Invoice.php";
+include_once LIB_PATH . "/FreshBooks/Project.php";
 
 //new Client object
-$invoice = new FreshBooks_Invoice();
+$project = new FreshBooks_Project();
 
-// populate properties
-$invoice->clientId = 142596;
+//listing function definition void   listing  ( &$rows,  &$resultInfo, [ $page = 1], [ $perPage = 25], [ $filters = array()])
+//get projects, 25 per page, page 1, where project belong to client id 3
+$project->listing($rows,$resultInfo,1,25,array('client_id'=>0));
 
-//lines (items) is array of asoc. arrays
-$invoice->lines = array(
-    //1st item
-    array(
-        'name' => 'xyz',
-        'unitCost' => 99.99
-        //all other required properties should be populated
-    ),
-    //2nd item
-    array(
-        'name'=>'yyy',
-        'unitCost'=>199.99,
-    )
-);
-
-//try to get client with client_id $clientId
-if(!$invoice->create()){
-	//no data - read error
-	echo $invoice->lastError;
-} else {
-	//investigate populated data
-	print_r($invoice);
-	echo "<hr>";
-	echo $invoice->invoice_id;
-}
+//dump result data
+print_r($rows);
+print_r($resultInfo);
