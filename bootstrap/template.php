124f2a1c2f4cbf929843ae8695692bae6c353987
scubacoder <vinceaycardo@gmail.com>
2014-11-24 23:31:10 +0800

    bootstrap navbar

---
+++ bootstrap/template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>HealthTrust Software</title>
+	</head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="bs/css/bootstrap.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="bs/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="bs/js/bootstrap.min.js"></script>
+<body>
+
+Stuff
+</body>
+</html>
